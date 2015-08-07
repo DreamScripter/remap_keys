3eefe6b9576c74836550285e9988c9ad6990ef1e
DreamScripter <DreamScripter@hushmail.com>
2015-08-07 12:15:21 +0200

    Linux part made

--- LINUX/remap_keyboard_spanish_master.sh
+++ LINUX/remap_keyboard_spanish_master.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-setxkbmap -option caps:swapescape
+OLD_PWD=${PWD}
+DIR=/mnt/invar/remap_keys/LINUX/super-caps
 
-# DIR=/mnt/invar/remap_keys/LINUX
-
-# primero, las teclas que se intercambian, con el programa sencillo, xmodmap
-# nohup ${DIR}/remap_keyboard_spanish_xmodmap.sh > /dev/null 2>&1 &
-
-# luego, ajuste fino de tecla altgr, y por tipo de aplicación con autokey
-# nohup autokey > /dev/null 2>&1 &
-
+cd ${DIR}
+./super-caps.sh
+cd ${OLD_PWD}
